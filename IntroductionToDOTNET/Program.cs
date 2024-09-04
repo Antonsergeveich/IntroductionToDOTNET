6bb3fd67188ed1fb4e626cb089385e43a47a058f
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-04 19:36:15 +0300

    +

--- IntroductionToDOTNET/Program.cs
+++ IntroductionToDOTNET/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define OUTPUT_TO_SCREEN
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace IntroductionToDOTNET
     {
         static void Main(string[] args)
         {
+#if OUTPUT_TO_SCREEN
             Console.Title = "Introduction to .NET";// Titel задаёт заголовок окна консоли
             //Console.Write("Hello .NET");   // выводит строку на экран
             Console.WriteLine("Hello .NET"); //выводит строку на экран и переходит вначало новой строки
@@ -22,9 +24,11 @@ namespace IntroductionToDOTNET
             Console.WriteLine("Вот так задаются координаты");
             Console.ResetColor(); // применяет цветовую схему по умолчанию
 
+            #region ONE
             //Задаём размеры окна:
             //Console.WindowWidth = 64;
-            //Console.WindowHeight = 16;
+            //Console.WindowHeight = 16; 
+            #endregion
             Console.SetWindowSize(64, 16);
             //Задаём положение окна на экране:
             // Console.WindowLeft = 10; 
@@ -34,7 +38,33 @@ namespace IntroductionToDOTNET
 
             //Задаём размер буфера консоли:
             Console.BufferWidth = Console.WindowWidth;
-            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferHeight = Console.WindowHeight; 
+#endif
+            Console.Write("Ввеедите Ваше имя: ");
+            string first_name = Console.ReadLine(); // метод ReadLine() читает строку с клавиатуры до нажатия Enter
+            // И возвращает прочитанную строку;
+            //Console.WriteLine(first_name);
+
+            Console.Write("Введите Вашу фамилию: ");
+            string last_name = Console.ReadLine();
+            //Console.WriteLine(last_name);
+
+            Console.Write("Введите Ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            /*
+              -----------------------------------------------
+            Класс Convert представляет собой набор статических методов для преобразования типов.
+            Этот класс используется как правило в том случае, когда другие преобразования не работают.
+            ---------------------------------------------------
+            */
+            //Console.WriteLine(a);
+            // Операции над строками:
+            Console.WriteLine(last_name + " " + first_name + " " +  age + " years"); //Конкатенация строк
+            // Кроме конкатенации строк C# поддерживает форматирование строк:
+            Console.WriteLine(String.Format("{0} {1} {2} years", last_name, first_name, age));
+
+            //Самая удобная операция - Интерполяция строк:
+            Console.WriteLine($"{last_name} {first_name} {age} years");
         }
     }
 }
