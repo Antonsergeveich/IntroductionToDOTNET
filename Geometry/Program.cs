f10813a7921dd4a0692e8d8f98da44ee266a854c
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-06 23:01:50 +0300

    +

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,48 @@ namespace Geometry
 {
     internal class Program
     {
+        private static int n;
+        static readonly string delimeter = "\n======================================\n";
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Введите количество звёздочек: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("0)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) Console.Write('*');
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("1)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++) Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("2)");
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = n; j > 0; j--) Console.Write(j <= i ? "*" : "");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("3)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) Console.Write(j < i ? " " : "*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("4)");
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = 0; j < n; j++) Console.Write(j < i - 1 ? " " : "*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
