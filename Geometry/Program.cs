86b995aa3c4d965ace257567d3e06b2e6d920204
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-07 00:05:14 +0300

    +

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -60,9 +60,10 @@ namespace Geometry
             for(int i = 0; i < n; i++)
             {
                 for (int j = 0; j <= i; j++) Console.Write(j <= i - 1 ? " ": "\\");
-                for(int j = 0; j < (n - 1 - i) * 2; j++)Console.Write(j < (n - 1 - i) * 2 - 1 ? " " : "/");    
+                for(int j = 0; j <= (n - 1 - i) * 2; j++)Console.Write(j < (n - 1 - i) * 2  ? " " : "/");    
                 Console.WriteLine();
             }
+            Console.WriteLine();
             Console.WriteLine("6)");
             for (int i = 0; i < n; i++)
             {
