2fb0b600228eb5cae4aa7fbbe52a4628523f6ec8
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-06 21:34:25 +0300

    +

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define LITERALS
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -56,12 +57,64 @@ namespace DataTypes
             //Console.WriteLine($"и принимает значения в диапазоне: {Int64.MinValue} ... {Int64.MaxValue}");
             //Console.Write($"Диапазон принимаемых значений 'Unit64': {UInt64.MinValue} ... {UInt64.MaxValue};");
             //Console.WriteLine();
-            //Console.WriteLine(delimeter2); 
+            //Console.WriteLine(delimeter2);
+
+            // Console.Write($"Переменная типа 'float' занимает {sizeof(float)} Байт,");
+            // Console.WriteLine($"и принимает значения в диапазоне: {Single.MinValue} ... {Single.MaxValue}");
+            // Console.Write($"Диапазон принимаемых значений 'ulong': {Single.MinValue} ... {Single.MaxValue};");
+            // Console.WriteLine(delimeter1);
+            // Console.Write($"Переменная типа 'Int64' занимает {sizeof(Int64)} Байт,");
+            // Console.WriteLine($"и принимает значения в диапазоне: {Int64.MinValue} ... {Int64.MaxValue}");
+            // Console.Write($"Диапазон принимаемых значений 'Unit64': {UInt64.MinValue} ... {UInt64.MaxValue};");
+            // Console.WriteLine();
+            // Console.WriteLine(delimeter2); 
             #endregion
 
-            double a = 12.56;
-            Console.WriteLine(a*100000);
+            //double a = 12.56;
+            //Console.WriteLine(a*100000);
+#if LITERALS
+            Console.WriteLine(123.GetType().Name);     //int
+            Console.WriteLine(123.4f.GetType().Name);  //float
+            Console.WriteLine(123f.GetType().Name);    //float
+            Console.WriteLine(123.4.GetType().Name);   //double
+            Console.WriteLine(5d.GetType().Name);   //double
+            Console.WriteLine(123.4m.GetType().Name);  //decimal
+            Console.WriteLine(123m.GetType().Name);    //decimal
+            Console.WriteLine(123L.GetType().Name);    //long
+            Console.WriteLine(123ul.GetType().Name);   //long
+            Console.WriteLine(delimeter1);
+            Console.WriteLine('+'.GetType());
+            Console.WriteLine("Hello".GetType()); \
+#endif
+            //Type conversion или type cast
+            #region Conversions1
+            //Console.WriteLine(((byte)5).GetType().Name);
+            ////C-like notation: (type)value;
+            //int a = 2;
+            //int b = 3;
+            //Console.WriteLine(a+"\t"+b);
+            //a = (int)b;
+            //Console.WriteLine(a+"\t"+b);
+            //byte c = 2;
+            //uint d = uint.MaxValue;
+            //c = (byte)d; //Truncation; 
+            #endregion
 
+            try
+            {
+                int a = int.MaxValue;
+                uint b = uint.MaxValue;
+                Console.WriteLine(a + b);
+                Console.WriteLine((a + b).GetType().Name);
+                long c = long.MaxValue;
+                Console.WriteLine(b + c);
+                Console.WriteLine((b + c).GetType());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Buy");
         }
     }
 }
