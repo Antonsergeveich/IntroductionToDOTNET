88f52aebc4724cd380ccb227583ea6c0b7603be6
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-08 17:55:32 +0300

    factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число до 20 включительно для вычисления факториала: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            long f = 1; //Факториал
+            for (int i = 1; i <= n; i++)
+            {
+                f *= i;
+            }
+            Console.WriteLine($"{n}! = {f}");
+            Console.WriteLine();
+        }
+    }
+}
