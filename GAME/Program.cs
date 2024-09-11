5728032f65ead6d9c56fb7caad179cee5c9eb2e9
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-11 15:19:58 +0300

    +

---
+++ GAME/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //char key;
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey(true).Key;
+                //Console.WriteLine(key);
+                //ConsoleKey.
+                Console.WriteLine(key.ToString());
+            }while (key != ConsoleKey.Escape);
+        }
+    }
+}
