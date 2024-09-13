7383b9e5a7660ebed4cb7c0810f7b020f3c628b7
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-13 10:26:42 +0300

    +

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -1,7 +1,7 @@
 ﻿//#define STARS
 //#define HARDCHESS
 #define HARDCHESS2
-//#define CHESSBOARD
+#define CHESSBOARD
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -19,57 +19,57 @@ namespace Geometry
 
 #if STARS
             #region ST
-            //Console.WriteLine("Введите количество звёздочек: ");
-            //int n = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("0)");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++) Console.Write('*');
-            //    Console.WriteLine(" ");
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("1)");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j <= i; j++) Console.Write("*");
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("2)");
-            //for (int i = n; i > 0; i--)
-            //{
-            //    for (int j = n; j > 0; j--) Console.Write(j <= i ? "*" : "");
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("3)");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++) Console.Write(j < i ? " " : "*");
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("4)");
-            //for (int i = n; i > 0; i--)
-            //{
-            //    for (int j = 0; j < n; j++) Console.Write(j < i - 1 ? " " : "*");
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("5)"); 
+            Console.WriteLine("Введите количество звёздочек: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("0)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) Console.Write('*');
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("1)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++) Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("2)");
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = n; j > 0; j--) Console.Write(j <= i ? "*" : "");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("3)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) Console.Write(j < i ? " " : "*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("4)");
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = 0; j < n; j++) Console.Write(j < i - 1 ? " " : "*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("5)");
             #endregion
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = i; j < n; j++) Console.Write(j < n - 1 ? " " : "/");
-            //    for (int j = 0; j <= i * 2; j++) Console.Write(j < i * 2 ? " " : "\\");
-            //    Console.WriteLine();
-            //}
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j <= i; j++) Console.Write(j <= i - 1 ? " " : "\\");
-            //    for (int j = 0; j <= (n - 1 - i) * 2; j++) Console.Write(j < (n - 1 - i) * 2 ? " " : "/");
-            //    Console.WriteLine();
-            //}
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++) Console.Write(j < n - 1 ? " " : "/");
+                for (int j = 0; j <= i * 2; j++) Console.Write(j < i * 2 ? " " : "\\");
+                Console.WriteLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++) Console.Write(j <= i - 1 ? " " : "\\");
+                for (int j = 0; j <= (n - 1 - i) * 2; j++) Console.Write(j < (n - 1 - i) * 2 ? " " : "/");
+                Console.WriteLine();
+            }
 
             Console.Write("Введите размер фигуры: ");
             int x = Convert.ToInt32(Console.ReadLine());
@@ -83,16 +83,16 @@ namespace Geometry
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine(); 
+            Console.WriteLine();
 
 
-            //Console.WriteLine("6)");
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++) Console.Write((i + j) % 2 == 0 ? "+ " : "- ");
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine();
+            Console.WriteLine("6)");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++) Console.Write((i + j) % 2 == 0 ? "+ " : "- ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
 #endif
 
 #if HARDCHESS
